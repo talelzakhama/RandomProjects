72bbc1f1daa935b22081227c35c44e6147bf6f57
Talel Zakhama <talelzakhama@MacBook-Pro-2.local>
2022-03-29 00:00:38 -0700

    Editing Finding Regex

--- FindingRegex.py
+++ FindingRegex.py
@@ -1,21 +1,25 @@
 import re
 
 # Read the txt file.
-infile =r"/Users/talelzakhama/Desktop/Test.log"
+# infile =r"/Users/talelzakhama/Desktop/Test.log"
 
-#Read the txt file.
-with open (infile) as f:
-	f=f.readlines()
+def FindWord(text,word):
+	#Read the txt file.
+	with open (text) as f:
+		f=f.readlines()
+	# Converting the list to a string
+	fstr= str(f)
+	#Searching for the string of interest, here it's "Passed"
+	re.search (word,fstr)
+	# Listing all found strings of interest.
+	all_found = re.findall(word,fstr)
+	return len(all_found)
 
-# Converting the list to a string
-fstr= str(f)
 
-word = input ("Please enter the word you're looking for below\n")
 
-#Searching for the string of interest, here it's "Passed"
-re.search (word,fstr)
+# print ("Keyword ""{}"" was found {} times\nAdios Amigo".format(word,len(all_found)))
 
-# Listing all found strings of interest.
-all_found = re.findall("Passed",fstr)
-
-print ("Keyword ""{}"" was found {} times\nAdios Amigo".format(word,len(all_found)))
+if __name__=="__main__":
+	path = input("Please enter path to text:\n")
+	string = input ("Please enter the word you're looking for below\n")
+	print (f"Keyword '{string}' was found {FindWord(path,string)} times\nAdios Amigo")
